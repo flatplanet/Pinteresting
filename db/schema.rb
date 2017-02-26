3dc1fab02e0cb54cf7ab12f3549a87d5e0245c00
John Elder <johne4196@gmail.com>
2017-02-26 18:53:11 +0000

    added paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225161244) do
+ActiveRecord::Schema.define(version: 20170226172459) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
