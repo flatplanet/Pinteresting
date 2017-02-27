b45b353e510be2b1d327d714e27643a3971b0a90
John Elder <johne4196@gmail.com>
2017-02-27 14:00:27 +0000

    added aws-sdk

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'devise', '~> 4.2'
 gem 'paperclip', '~> 5.1'
+gem 'aws-sdk', '~> 2.7', '>= 2.7.15'
 
 
 group :development, :test do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.default_url_options = { host: 'thawing-savannah-29285.herokuapp.com'}
+  
+  config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
+}
 end
