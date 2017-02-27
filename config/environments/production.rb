27275d93d47a92b9a0d8de69f36f69f66ea2aca1
John Elder <johne4196@gmail.com>
2017-02-27 14:13:26 +0000

    tweaked aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,6 @@ Rails.application.configure do
   
   config.paperclip_defaults = {
   storage: :s3,
-  :s3_host_name => "s3-us-west-2.amazonaws.com", 
-  :url => ":s3_host_name",                        
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
@@ -91,4 +89,6 @@ Rails.application.configure do
   }
   
 }
+
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
 end
